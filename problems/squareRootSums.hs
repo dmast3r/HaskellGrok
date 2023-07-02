55c5bf382ebc8a61b126eca93805cdbd0323b7ef
dmast3r <sorbkhandelwal@gmail.com>
2023-07-02 21:05:36 +0530

    using scanl1

---
+++ problems/squareRootSums.hs
@@ -0,0 +1,6 @@
+{-
+    Find how many elements does it take for the sum of the roots of all natural numbers to exceed n
+-}
+
+-- Todo: write the method signature
+squareRootSums n = length (takeWhile (<= n) $ scanl1 (+) $ map sqrt [1..]) + 1
